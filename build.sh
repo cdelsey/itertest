61e17fc27137ab9535991d186bbfe621f805a104
Carl Delsey <carl.r.delsey@intel.com>
2016-11-11 16:21:09 -0800

    Initial empty build.

---
+++ build.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+CURR_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+SRC_DIR=${CURR_DIR}/src
+BUILD_DIR=${CURR_DIR}/output/debug
+NUMJOBS=$(expr `nproc` \* 2)
+
+set -e
+mkdir -p $BUILD_DIR
+cd $BUILD_DIR
+cmake -DCMAKE_BUILD_TYPE=Debug $SRC_DIR
+make -j${NUMJOBS}
