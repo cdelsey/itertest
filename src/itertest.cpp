4cc9fbf3664be84941b3f0d5423272762053d30e
Carl Delsey <carl.r.delsey@intel.com>
2016-11-11 17:37:39 -0800

    Initialize vector with sequence of ints.

--- src/itertest.cpp
+++ src/itertest.cpp
@@ -1,3 +1,16 @@
+#include <vector>
+#include <iterator>
+#include <numeric>
+#include <iostream>
+
+using namespace std;
+
 int main(int argc, char* argv[]) {
+    ios::sync_with_stdio(false);
+    vector<int> cam(8);
+    iota(begin(cam), end(cam), 0);
+    for(auto i : cam)
+        cout << i;
+    cout << '\n';
     return 0;
 }
